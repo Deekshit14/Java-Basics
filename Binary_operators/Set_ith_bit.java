d76f646a1fc13d7a24043f5476568e274378c92f
Deekshit M <deekshith1404@gmail.com>
2024-03-03 15:23:18 +0530

    Solution for reseting the ith bit

--- Binary_operators/Set_ith_bit.java
+++ Binary_operators/Set_ith_bit.java
@@ -5,11 +5,12 @@ package Binary_operators;
 import java.util.Scanner;
 public class Set_ith_bit {
     public static void main(String[] args) {
-        int num = 19;  // 10110110 in bit form
+        int num = 19;  // 10011 in bit form
         Scanner s = new Scanner(System.in);
         System.out.println("Enter the ith bit to find");
         int i = s.nextInt();
         System.out.println(Set_ith_bit(num, i));
+        // Output will be 23 when it is provided ith bit is given 3, i.e (10011 to 11011) after setting ith bit
     }   
 
     public static int Set_ith_bit(int num, int i)
--- Binary_operators/reset_ith_bit.java
+++ Binary_operators/reset_ith_bit.java
@@ -1,16 +1,19 @@
 package Binary_operators;
+
+// This is used to reset ith bit to zero
 import java.util.Scanner;
-public class ith_index {
+public class reset_ith_bit {
     public static void main(String[] args) {
-        int num = 182;  // 10110110 in bit form
+        int num = 6;  // 10110110 in bit form
         Scanner s = new Scanner(System.in);
         System.out.println("Enter the ith bit to find");
         int i = s.nextInt();
         System.out.println(find_ith_bit(num, i));
+        // The answer would be 4 i.e, (110 to 100) when ith bit is given 2
     }
 
     public static int find_ith_bit(int num, int i)
     {
-        return num & (1 << (i-1));
+        return num & (~(1 << (i-1)));
     }
 }
