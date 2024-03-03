a1752a9b0d6e0503db7e9d8d5757c18e1f2f86df
Deekshit M <deekshith1404@gmail.com>
2024-03-03 14:49:18 +0530

    Solotion for ith bit

---
+++ Binary_operators/ith_index.java
@@ -0,0 +1,16 @@
+package Binary_operators;
+import java.util.Scanner;
+public class ith_index {
+    public static void main(String[] args) {
+        int num = 182;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the ith bit to find");
+        int i = s.nextInt();
+        System.out.println(find_ith_bit(num, i));
+    }
+
+    public static int find_ith_bit(int num, int i)
+    {
+        return num & (1 << (i-1));
+    }
+}
